371ff965072fb9f88932f9927effbd7c26c95a99
Tilan Chicha <tchicha@utbm.fr>
2018-10-15 14:26:40 +0200

    test

---
+++ src/app/pizza-details/pizza-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Pizza } from '../pizza';
+import { ActivatedRoute } from '@angular/router';
+import { PizzaService } from '../pizza.service';
+
+@Component({
+  selector: 'app-pizza-details',
+  templateUrl: './pizza-details.component.html',
+  styleUrls: ['./pizza-details.component.css']
+})
+export class PizzaDetailsComponent implements OnInit {
+
+  pizza: Pizza;
+  constructor(private activateRoute: ActivatedRoute,private pizzaService: PizzaService) { }
+
+  ngOnInit() {
+      this.getPizza();
+  }
+
+  getPizza(){
+      const id = +this.activateRoute.snapshot.paramMap.get('id');
+      this.pizzaService.PizzaByID(id)
+          .subscribe(pizza => this.pizza = pizza);
+    }
+}
